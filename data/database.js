0eb2d97db5576203cc2ec0a34283cc4ce3d53565
Ivyson13 <ivyson12@gmail.com>
2023-11-03 18:55:50 -0300

    primeiro commit

---
+++ data/database.js
@@ -0,0 +1,12 @@
+let candidatos = [
+    {
+        id: 1,
+        nome: 'Teste',
+        contato: 'teste@teste.com',
+        habilidades: 'teste teste',
+        status: 'Não contratado',
+        contratacao_data:''
+    }
+]
+
+module.exports = candidatos
